34e97ff86807a7263f4593575c4581b1918c52ac
gionesei <ioneseigabriel@gmail.com>
2025-10-28 09:53:20 +0200

    commit3

--- work/src/main/java/org/example/Main.java
+++ work/src/main/java/org/example/Main.java
@@ -13,5 +13,12 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+
+        for (int i = 1; i <= 15; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("j = " + i);
+        }
     }
 }
